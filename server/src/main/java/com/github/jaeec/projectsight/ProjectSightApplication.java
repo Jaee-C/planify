9d3ab6612d86bbad4e1666a1ad0c4e132cb2277a
Daniel Chin <wengjae@gmail.com>
2023-04-22 00:17:32 +1000

    add spring project

---
+++ server/src/main/java/com/github/jaeec/projectsight/ProjectSightApplication.java
@@ -0,0 +1,13 @@
+package com.github.jaeec.projectsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSightApplication.class, args);
+	}
+
+}
