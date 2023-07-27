47ef7ff0b9d9af5d607e58ad30184c152ef34678
Daniel Chin <wengjae@gmail.com>
2023-07-27 18:49:24 +1000

    remove placeholders

--- src/components/ProjectSettings/Details/Content.tsx
+++ src/components/ProjectSettings/Details/Content.tsx
@@ -8,7 +8,6 @@ export default function Content(): JSX.Element {
   return (
     <div className={styles.container}>
       <div>
-        <h3>CONTENT</h3>
         <Form />
       </div>
     </div>
