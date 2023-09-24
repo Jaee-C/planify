a45f9706f97ca62b426c204fb110f9161047667c
Jaee-C <77240633+Jaee-C@users.noreply.github.com>
2023-09-24 15:25:49 +1000

    update top navbar for board

--- src/components/Backlog/index.tsx
+++ src/components/Backlog/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { QueryClientProvider } from "react-query";
 import BacklogTable from "@/components/Backlog/BacklogTable";
 import { createContext, useState } from "react";
 import SideIssueViewer from "@/components/SideIssueViewer";
@@ -7,7 +6,6 @@ import { Button } from "@mui/material";
 import TableToolbar from "@/components/Table/TableToolbar";
 import { IconContext } from "react-icons";
 import TableAlert from "@/components/Backlog/TableAlert";
-import { queryClient } from "@/lib/client-data/query";
 
 interface SidebarContextProps {
   action: (id: string) => void;
